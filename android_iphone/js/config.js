85f5ce74a8c6d03ab20ee273033693ebe9f7d12a
ÖìС²¨ <764724624@qq.com>
2014-07-18 16:49:32 +0800

    重新布局 功能划分 设置配置文件 打包注意选键盘压缩模式解决输入框遮盖问题

--- android_iphone/js/config.js
+++ android_iphone/js/config.js
@@ -20,3 +20,19 @@ var group_url =  "http://"+ip+"/group.php?jsoncallback=?";
 var portal_url =  "http://"+ip+"/portal.php?jsoncallback=?";
 var newest_url =	"http://"+ip+"/newest.php?jsoncallback=?";
 var uc_url =		"http://"+ip+"/uc.php?jsoncallback=?";
+
+var soft_down_url = "http://iclouds.qiniudn.com/apk%E6%A0%A1%E5%9B%AD%E6%B1%87.apk";
+var share_content="校园汇App，便捷校园生活！";
+var share_pic="http://iclouds.qiniudn.com/pic%E6%A0%A1%E5%9B%AD%E6%B1%87.png";
+
+
+var surl = "http://xlanlab.com/index.php?app=jwc&mod=Index&act=getcheckimage";
+var login_url = "http://xlanlab.com/index.php?app=jwc&mod=Index&act=login_json";
+var course_url = "http://xlanlab.com/index.php?app=jwc&mod=Index&act=course&callback=json";
+var score_url = 'http://xlanlab.com/index.php?app=jwc&mod=Index&act=score&callback=json';
+var checklogin_url = "http://xlanlab.com/index.php?app=jwc&mod=Index&act=checkhaslogin";
+var loginout_url="http://xlanlab.com/index.php?app=jwc&mod=Index&act=logout";
+var evaluation_url="http://xlanlab.com/index.php?app=jwc&mod=Index&act=evaluation";
+var notice_url="http://xlanlab.com/index.php?app=jwc&mod=Index&act=jwtzDetail";
+var jwtz_url  = 'http://xlanlab.com/index.php?app=jwc&mod=Index&act=jwtz';
+var jwc_score_url  = 'http://xlanlab.com/index.php?app=jwc&mod=Index&act=score&callback=json';
